3efac9581be83180f355654e1c65feacd319aa0f
guy cao <guycao@gmail.com>
2014-06-04 10:45:48 -0700

    reworded test description. add test for error message

--- spec/controllers/api/lists_controller_spec.rb
+++ spec/controllers/api/lists_controller_spec.rb
@@ -1,39 +1,41 @@
 require 'spec_helper'
 
 describe Api::ListsController do 
-  describe "#create" do
 
+  describe "#create" do
     context "with correct user's password" do
 
-      it "takes a list name, creates it if it doesn't exist" do
+      it "creates list succesfully" do
         user = FactoryGirl.create(:user)
         json = {:user => {:username => user.username, :password => user.password}, 
           :list => {:name => "Shopping List", :user_id => user.id, :permissions => "private"}}
         expect{ post :create, json }.to change{ List.count }.by 1
       end
 
-      it "takes a list name and will return false if it already exists" do
+      it "cannot create list if list name already exists" do
         user = FactoryGirl.create(:user)
         list = FactoryGirl.create(:list, user_id: user.id)
         json = {:user => {:username => user.username, :password => user.password},
           :list => {:name => list.name, :user_id => user.id, :permissions => "private"}}
         expect{ post :create, json }.to_not change{ List.count }.by 1
         expect(response.body).to include "List was not created"
-      end
-
-      context "without correct user's password" do
-        it "it errors"
+        expect(assigns(:list).errors.messages.to_s).to include "List name already exists"
       end
     end
 
-    describe "index" do
-      context "with correct user's password" do
-        it "returns all lists associated with the user"
-      end
-
-      context "without correct user's password" do
-        it "returns all visible and open lists"
-      end
+    context "without correct user's password" do
+      it "it errors"
     end
   end
+
+  describe "index" do
+    context "with correct user's password" do
+      it "returns all lists associated with the user"
+    end
+
+    context "without correct user's password" do
+      it "returns all visible and open lists"
+    end
+  end
+
 end
